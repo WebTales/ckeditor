c19a96f270941fb9df2329bec7696b7b19fd95a2
Alexandru Dobre <alexdobre89@gmail.com>
2013-04-17 11:36:52 +0200

    size fix

--- plugins/rubedolink/plugin.js
+++ plugins/rubedolink/plugin.js
@@ -3,7 +3,7 @@ CKEDITOR.plugins.add( 'rubedolink', {
     init: function( editor ) {
         editor.addCommand( 'insertRubedolink', {
             exec: function( editor ) {
-            		editor.popup("link-finder?ckinstance="+editor.name,200,200);
+            		editor.popup("link-finder?ckinstance="+editor.name,"300px","300px");
             }
         });
         editor.ui.addButton( 'Rubedolink', {
